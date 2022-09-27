56fbec889eef61662cb53f5abe92271b2b757bea
Jules Muhizi <jmuhizi@college.harvard.edu>
2022-09-27 18:56:43 +0000

    fixed depth_to_space

--- sandbox/lambda.cpp
+++ sandbox/lambda.cpp
@@ -5,6 +5,15 @@
 ///////////////////////////////////////////////////////////
 //////helper functions for debugging, not for synthesis////
 ///////////////////////////////////////////////////////////
+void helper_sum_vector (std::vector<int>vec){
+    int N = vec.size();
+    int counter = 0;
+    for (int n = 0; n < N; n++) {
+        counter += vec[n];
+    }
+    std::cout << "size of vector is: " << counter << "\n";
+}
+
 void helper_sum_img (std::vector<std::vector<std::vector<std::vector<int>>>> img){
     //get incoming image dimensions
     int N = img.size();
@@ -33,15 +42,16 @@ void helper_print_4x4 (std::vector<std::vector<std::vector<std::vector<int>>>> i
     int H = img[0].size();
     int W = img[0][0].size();
     int C = img[0][0][0].size();
-    assert(C > 4 && W > 4);
-    std::cout << N;
+    
+    std::cout << "N: " << N;
     std::cout << "\n";
-    std::cout << H;
+    std::cout << "H: " << H;
     std::cout << "\n";
-    std::cout << W;
+    std::cout << "W: " << W;
     std::cout << "\n";
-    std::cout << C;
+    std::cout << "C: " << C;
     std::cout << "\n";
+    assert(C >= 4 && W >= 4);
 
     for (int i = 0; i < 4; i++)
     {
@@ -99,16 +109,18 @@ std::vector<std::vector<std::vector<std::vector<int>>>> depth_to_space(std::vect
     assert (block_size*block_size <= C);
     std::vector<int> tmp_list(N*H*W*C);
 
-
+    //Copy block_size x block_size chunks of data into a temporary buffer
+    int counter  = 0;
     std::vector<std::vector<std::vector<std::vector<int>>>> img_out(N,std::vector<std::vector<std::vector<int>>>(new_H,(std::vector<std::vector<int>>(new_W, std::vector<int>(new_C, 0)))));
     for (int n = 0; n < N; n++) {
         for (int h = 0; h < H; h++) {
-            for (int w = 0; w < W; w++) {
-                for (int c = 0; c < C; c++) {
-                    for (int x =0; x< block_size; x++){
-                        for (int y = 0; x < block_size; x++) {
+            for (int w = 0; w < W; w+=block_size) {
+                for (int c = 0; c < C; c+=block_size) {
+                    for (int x = 0; x < block_size; x++){
+                        for (int y = 0; y < block_size; y++) {
                             if (w+x < W && c + y < C) {
                                 tmp_list.push_back(img[n][h][w+x][c+y]);
+                                counter += img[n][h][w+x][c+y];
                             }
                         }
                     }
@@ -117,41 +129,29 @@ std::vector<std::vector<std::vector<std::vector<int>>>> depth_to_space(std::vect
         }
     }
 
-    //remap the intermediate list with the correct indices
-    //remap the first 216
-    for (int counter=0; counter <216; counter++){
-        for (auto it = img_out.begin(); it != img_out.end(); ++it){
-            for (auto it1 = it->begin(); it1 != it->end(); ++it1){
-                for (auto it2 = it1->begin(); it2 != it1->end(); ++it2){
-                    for (auto it3 = it2->begin(); it3 != it2->end(); ++it3){
-                        *it3 = tmp_list[counter*3]; 
-                    }
-                }
-            }
+    //re-shuffle the buffer
+    std::vector<int> tmp_list1(N*H*W*C);
+    for (int h = 0; h <H; h++){
+        int HEIGHT_OFFSET = W * h;
+        int SHUFFLE = W/3; //   W/block_size (in the future?)
+        for (int s = 0; s < SHUFFLE; s++){
+            tmp_list1.push_back(tmp_list[s*3+HEIGHT_OFFSET]);
+        }
+        for (int s = SHUFFLE; s < SHUFFLE*2; s++){
+            tmp_list1.push_back(tmp_list[(s-SHUFFLE)*3+HEIGHT_OFFSET]);
+        }
+        for (int s = SHUFFLE*2; s < SHUFFLE*3; s++){
+            tmp_list1.push_back(tmp_list[(s-2*SHUFFLE)*3+ 2*HEIGHT_OFFSET]);
         }
     }
 
-    //remap the next 216
-    for (int counter=216; counter <432; counter++){
-        for (auto it = img_out.begin(); it != img_out.end(); ++it){
-            for (auto it1 = it->begin(); it1 != it->end(); ++it1){
-                for (auto it2 = it1->begin(); it2 != it1->end(); ++it2){
-                    for (auto it3 = it2->begin(); it3 != it2->end(); ++it3){
-                        *it3 = tmp_list[(counter-216)*3+1]; 
-                    }
-                }
-            }
-        }
-    }
-
-    //remap the next 216
-    for (int counter=432; counter <648; counter++){
-        for (auto it = img_out.begin(); it != img_out.end(); ++it){
-            for (auto it1 = it->begin(); it1 != it->end(); ++it1){
-                for (auto it2 = it1->begin(); it2 != it1->end(); ++it2){
-                    for (auto it3 = it2->begin(); it3 != it2->end(); ++it3){
-                        *it3 = tmp_list[(counter-432)*3+2]; 
-                    }
+    //Stream the reshuffled flat vector into the correct output dimensions
+    int count = 0;
+    for (int n = 0; n < N; n++) {
+        for (int h = 0; h < new_H; h++) {
+            for (int w = 0; w < new_W; w++) {
+                for (int c = 0; c < new_C; c++) {
+                    img_out[n][h][w][c] = tmp_list1[count++];
                 }
             }
         }
@@ -189,7 +189,6 @@ std::vector<std::vector<std::vector<std::vector<int>>>> clip_img (std::vector<st
     int C = img[0][0][0].size();
 
     //set outgoing image dimensions
-    // int img_out[N][H][W][C];
     std::vector<std::vector<std::vector<std::vector<int>>>> img_out(N,std::vector<std::vector<std::vector<int>>>(H,(std::vector<std::vector<int>>(W, std::vector<int>(C, 0)))));
     for (int n = 0; n < N; n++) {
         for (int h = 0; h < H; h++) {
@@ -204,10 +203,9 @@ std::vector<std::vector<std::vector<std::vector<int>>>> clip_img (std::vector<st
 }
 
 int main() {
-    int N = 1, H = 40, W = 4, C = 4;
-    std::vector<std::vector<std::vector<std::vector<int>>>> img(N,std::vector<std::vector<std::vector<int>>>(H,(std::vector<std::vector<int>>(W, std::vector<int>(C, 200)))));
+    int N = 1, H = 1, W = 648, C = 9;
+    std::vector<std::vector<std::vector<std::vector<int>>>> img(N,std::vector<std::vector<std::vector<int>>>(H,(std::vector<std::vector<int>>(W, std::vector<int>(C, 3)))));
     // print_4x4(img);
     // print_4x4(upsample_img(img, 3));
-    helper_print_4x4(add_img(img,img));
-
+    helper_sum_img(depth_to_space(img,3));
 }
