5ff5a3a5e845dc31077866f0d8ccc462fc248e28
Jules Muhizi <jmuhizi@college.harvard.edu>
2022-11-30 00:01:12 +0000

    format to separate layer and activation

--- solvers/networks/base7.py
+++ solvers/networks/base7.py
@@ -1,8 +1,10 @@
 import tensorflow as tf
-from tensorflow.keras.layers import Conv2D, Input, ReLU, Lambda, Add
+from tensorflow.keras.layers import Conv2D, Input, ReLU, Lambda, Add, Activation
+from keras import activations
 from qkeras import QConv2D
 from qkeras.quantizers import quantized_bits, quantized_relu
 from tensorflow.keras.models import Model
+from qkeras.qlayers import QActivation
 from tensorflow.keras.initializers import glorot_normal
 import tensorflow.keras.backend as K
 import numpy as np
@@ -15,13 +17,15 @@ def base7(scale=3, in_height=216, in_width=680, in_channels=3, num_fea=28, m=4, 
     upsampled_inp = upsample_func([inp]*(scale**2))
 
     # Feature extraction
-    x = Conv2D(num_fea, 3, padding='same', activation='relu', kernel_initializer=glorot_normal(), bias_initializer='zeros')(inp)
-
+    x = Conv2D(num_fea, 3, padding='same', kernel_initializer=glorot_normal(), bias_initializer='zeros')(inp)
+    x = Activation(activations.relu)(x)
     for i in range(m):
-        x = Conv2D(num_fea, 3, padding='same', activation='relu', kernel_initializer=glorot_normal(), bias_initializer='zeros')(x)
+        x = Conv2D(num_fea, 3, padding='same', kernel_initializer=glorot_normal(), bias_initializer='zeros')(x)
+        x = Activation(activations.relu)(x)
 
     # Pixel-Shuffle
-    x = Conv2D(out_channels*(scale**2), 3, padding='same', activation='relu', kernel_initializer=glorot_normal(), bias_initializer='zeros')(x)
+    x = Conv2D(out_channels*(scale**2), 3, padding='same', kernel_initializer=glorot_normal(), bias_initializer='zeros')(x)
+    x = Activation(activations.relu)(x)
     x = Conv2D(out_channels*(scale**2), 3, padding='same', kernel_initializer=glorot_normal(), bias_initializer='zeros')(x)
     x = Add()([upsampled_inp, x])
 
@@ -33,20 +37,22 @@ def base7(scale=3, in_height=216, in_width=680, in_channels=3, num_fea=28, m=4, 
     return Model(inputs=inp, outputs=out)
 
 def base7_quantized(scale=3, in_height=216, in_width=680, in_channels=3, num_fea=28, m=4, out_channels=3):
-    quantizer_kwargs = {'kernel_quantizer': quantized_bits(8,0,alpha=1),
-              'bias_quantizer': quantized_bits(8,0,alpha=1)}
+    quantizer_kwargs = {'kernel_quantizer': quantized_bits(5,0,alpha=1),
+              'bias_quantizer': quantized_bits(5,0,alpha=1)}
     inp = Input(shape=(in_height, in_width, 3))
     upsample_func = Lambda(lambda x_list: tf.concat(x_list, axis=3))
     upsampled_inp = upsample_func([inp]*(scale**2))
 
     # Feature extraction
-    x = QConv2D(num_fea, 3, padding='same', activation='relu', kernel_initializer=glorot_normal(), bias_initializer='zeros',**quantizer_kwargs)(inp)
+    x = QConv2D(num_fea, 3, padding='same', kernel_initializer=glorot_normal(), bias_initializer='zeros',**quantizer_kwargs)(inp)
+    x = QActivation(activation=quantized_relu(5,5))(x)
 
     for i in range(m):
-        x = QConv2D(num_fea, 3, padding='same', activation='relu', kernel_initializer=glorot_normal(), bias_initializer='zeros',**quantizer_kwargs)(x)
-
+        x = QConv2D(num_fea, 3, padding='same', kernel_initializer=glorot_normal(), bias_initializer='zeros',**quantizer_kwargs)(x)
+        x = QActivation(activation=quantized_relu(5,5))(x)
     # Pixel-Shuffle
-    x = QConv2D(out_channels*(scale**2), 3, padding='same', activation='relu', kernel_initializer=glorot_normal(), bias_initializer='zeros',**quantizer_kwargs)(x)
+    x = QConv2D(out_channels*(scale**2), 3, padding='same', kernel_initializer=glorot_normal(), bias_initializer='zeros',**quantizer_kwargs)(x)
+    x = QActivation(activation=quantized_relu(5,5))(x)
     x = QConv2D(out_channels*(scale**2), 3, padding='same', kernel_initializer=glorot_normal(), bias_initializer='zeros',**quantizer_kwargs)(x)
     x = Add()([upsampled_inp, x])
 
