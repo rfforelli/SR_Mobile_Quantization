0015695365e65e8155090ccdb98eb7a1368b91af
Jules Muhizi <jmuhizi@college.harvard.edu>
2022-11-30 00:09:45 +0000

    include updated train utils

--- AutoQKeras/train_utils.py
+++ AutoQKeras/train_utils.py
@@ -1,8 +1,11 @@
 from data import DIV2K
 import os.path as osp
 import shutil
-
+import math
+import numpy as np
 from tensorboardX import SummaryWriter
+import tensorflow.keras.backend as K
+
 
 
 def generate_train_data(args, lg):
@@ -26,3 +29,24 @@ def generate_train_data(args, lg):
     lg.info('Validating: [{}] iterations for each epoch'.format(len(val_data)))
 
     return (train_data, val_data)
+
+def calc_psnr(y, y_target):
+    def psnr_func(y, y_target):
+        h, w, c = y.shape
+        y = np.clip(np.round(y), 0, 255).astype(np.float32)
+        y_target = np.clip(np.round(y_target), 0, 255).astype(np.float32)
+
+        # crop 1
+        y_cropped = y[1:h-1, 1:w-1, :]
+        y_target_cropped = y_target[1:h-1, 1:w-1, :]
+        
+        mse = np.mean((y_cropped - y_target_cropped) ** 2)
+        if mse == 0:
+            return 100
+        return 20. * math.log10(255. / math.sqrt(mse))
+    
+    psnr = 0
+    for i, (sr, hr) in enumerate(zip(y, y_target)):
+        psnr += psnr_func((sr).squeeze(), (hr).squeeze())
+    psnr = round(psnr / len(y), 4)
+    return psnr
